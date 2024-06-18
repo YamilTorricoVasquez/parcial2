db32f74e9aea269500af3f2a2dd605e3b8768230
Yamil Torrico Vasquez <yamilvasqueztorrico@gmail.com>
2024-06-18 02:55:08 -0400

    W

--- controllers/boletin_estudiante.py
+++ controllers/boletin_estudiante.py
@@ -3,22 +3,32 @@ from odoo.http import request
 
 class BoletinController(http.Controller):
 
-    @http.route('/api/boletin/<ci>', type='json', auth='public', methods=['GET'], csrf=False, cors='*')
+    @http.route('/api/boletin/<string:ci>', type='json', auth='public', methods=['GET'], cors='*', csrf=False)
     def get_boletin_by_ci(self, ci):
-        # Fetch the boletin record by CI
-        boletin = request.env['academico.boletin'].sudo().search([('ci_estudiante', '=', ci)], limit=1)
-        if not boletin:
-            return {'error': 'No se encontró el boletín para el CI proporcionado'}
+        # Buscar el estudiante por CI
+        estudiante = request.env['academico.estudiante'].sudo().search([('ci', '=', ci)], limit=1)
+        
+        if not estudiante:
+            return {'error': 'No se encontró el estudiante para el CI proporcionado'}
+        
+        # Buscar el boletín utilizando el registro del estudiante
+        boletines = request.env['academico.boletin'].sudo().search([('estudiante_id', '=', estudiante.id)])
+        
+        if not boletines:
+            return {'error': 'No se encontró el boletín para el estudiante con CI proporcionado'}
 
-        # Prepare the response data
-        boletin_data = {
-            'estudiante': boletin.estudiante_id.name,
-            'ci_estudiante': boletin.ci_estudiante,
-            'curso': boletin.curso_id.name,
-            'nivel': boletin.nivel_id,
-            'estado_aprobacion': boletin.estado_aprobacion,
-            'promedio': boletin.promedio,
-            'notas': [{'asignatura': nota.asignatura_id.name, 'calificacion': nota.calificacion} for nota in boletin.nota_ids]
-        }
+        # Preparar los datos de la respuesta
+        boletin_data = []
+        for boletin in boletines:
+            boletin_info = {
+                'estudiante': boletin.estudiante_id.name,  # Nombre del estudiante
+                'ci_estudiante': boletin.ci_estudiante,
+                'curso': boletin.curso_id.name,
+                'nivel': boletin.nivel_id,
+                'estado_aprobacion': boletin.estado_aprobacion,
+                'promedio': boletin.promedio,
+                'notas': [{'asignatura': nota.asignatura_id.name, 'calificacion': nota.calificacion} for nota in boletin.nota_ids]
+            }
+            boletin_data.append(boletin_info)
 
         return boletin_data
