4af8a4aedd1bd10e55fd320a6da06dbab1db4e39
Yamil Torrico Vasquez <yamilvasqueztorrico@gmail.com>
2024-06-18 04:09:48 -0400

    D

--- controllers/boletin_estudiante.py
+++ controllers/boletin_estudiante.py
@@ -2,26 +2,47 @@ from odoo import http
 from odoo.http import request
 import json
 
-class BoletinAPI(http.Controller):
+class HelloApi(http.Controller):
 
-    @http.route('/api/boletin', auth='public', website=False, csrf=False, type='http', methods=['GET'], cors='*')
-    def get_boletines(self, **kw):
+    @http.route('/api/estudiantes', auth='public', website=False, csrf=False, type='http', methods=['GET'], cors='*')
+    def get_estudiantes(self, **kw):
         try:
-            boletines = request.env['academico.boletin'].sudo().search([])
-
-            boletin_data = []
-            for boletin in boletines:
-                boletin_data.append({
-                    'estudiante': boletin.estudiante_id.name,
-                    'ci_estudiante': boletin.ci_estudiante,
-                    'curso': boletin.curso_id.name,
-                    'nivel': boletin.nivel_id,
-                    'notas': [{'nombre': nota.name, 'valor': nota.valor} for nota in boletin.nota_ids],
-                    'estado_aprobacion': boletin.estado_aprobacion,
-                    'promedio': boletin.promedio,
+            estudiantes = request.env['academico.estudiante'].sudo().search([])
+            est_list = []
+            for est in estudiantes:
+                boletines = []
+                for boletin in est.boletin_ids:
+                    boletines.append({
+                        'curso': boletin.curso_id.name,
+                        'nivel': boletin.nivel_id,
+                        'estado_aprobacion': boletin.estado_aprobacion,
+                        'promedio': boletin.promedio,
+                    })
+                est_list.append({
+                    'name': est.name,
+                    'ci': est.ci,
+                    'email': est.email,
+                    'phone': est.phone,
+                    'curso_id': est.curso_id.name,
+                    'boletines': boletines
                 })
 
-            return json.dumps({'boletines': boletin_data})
+            return request.make_response(
+                json.dumps({
+                    'status': 200,
+                    'message': 'Success',
+                    'data': est_list
+                }),
+                headers={'Content-Type': 'application/json'}
+            )
 
         except Exception as e:
-            return json.dumps({'error': str(e)})
+            return request.make_response(
+                json.dumps({
+                    'status': 500,
+                    'message': f'Internal Server Error: {str(e)}',
+                    'data': []
+                }),
+                headers={'Content-Type': 'application/json'},
+                status=500
+            )
