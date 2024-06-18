bce930129fa447254c6961078dbe7d482ef7d6d0
Yamil Torrico Vasquez <yamilvasqueztorrico@gmail.com>
2024-06-18 16:55:07 -0400

    D

--- models/comunicado.py
+++ models/comunicado.py
@@ -4,7 +4,7 @@ class Comunicado(models.Model):
     _name = 'academico.comunicado'
     _description = 'Comunicado del colegio para padres'
 
-    name = fields.Char('Título', required=True)
-    contenido = fields.Html('Contenido')
-    fecha_publicacion = fields.Date('Fecha de Publicación')
+    name = fields.Char(string='Título', required=True)
+    contenido = fields.Text(string='Contenido')
+    fecha_publicacion = fields.Date(string='Fecha de Publicación')
     #visible_para_padres = fields.Boolean('Visible para Padres', default=True)
