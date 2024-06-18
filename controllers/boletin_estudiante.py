e5fcea32df11d48e0771cf41fe19d6ea3e181191
Yamil Torrico Vasquez <yamilvasqueztorrico@gmail.com>
2024-06-18 02:57:32 -0400

    P

--- controllers/boletin_estudiante.py
+++ controllers/boletin_estudiante.py
@@ -1,27 +1,34 @@
+import json
 from odoo import http
 from odoo.http import request
 
 class BoletinController(http.Controller):
 
-    @http.route('/api/boletin/<string:ci>', type='json', auth='public', methods=['GET'], cors='*', csrf=False)
-    def get_boletin_by_ci(self, ci):
+    @http.route('/api/boletin/<string:ci>', auth='public', website=False, csrf=False, type='http', methods=['GET'], cors='*')
+    def get_boletin_by_ci(self, ci, **kwargs):
         # Buscar el estudiante por CI
         estudiante = request.env['academico.estudiante'].sudo().search([('ci', '=', ci)], limit=1)
         
         if not estudiante:
-            return {'error': 'No se encontró el estudiante para el CI proporcionado'}
+            return request.make_response(
+                json.dumps({'error': 'No se encontró el estudiante para el CI proporcionado'}),
+                headers={'Content-Type': 'application/json'}
+            )
         
-        # Buscar el boletín utilizando el registro del estudiante
+        # Buscar los boletines utilizando el registro del estudiante
         boletines = request.env['academico.boletin'].sudo().search([('estudiante_id', '=', estudiante.id)])
         
         if not boletines:
-            return {'error': 'No se encontró el boletín para el estudiante con CI proporcionado'}
+            return request.make_response(
+                json.dumps({'error': 'No se encontró el boletín para el estudiante con CI proporcionado'}),
+                headers={'Content-Type': 'application/json'}
+            )
 
         # Preparar los datos de la respuesta
         boletin_data = []
         for boletin in boletines:
             boletin_info = {
-                'estudiante': boletin.estudiante_id.name,  # Nombre del estudiante
+                'estudiante': boletin.estudiante_id.name,
                 'ci_estudiante': boletin.ci_estudiante,
                 'curso': boletin.curso_id.name,
                 'nivel': boletin.nivel_id,
@@ -31,4 +38,7 @@ class BoletinController(http.Controller):
             }
             boletin_data.append(boletin_info)
 
-        return boletin_data
+        return request.make_response(
+            json.dumps({'status': 200, 'message': 'Success', 'data': boletin_data}),
+            headers={'Content-Type': 'application/json'}
+        )
