be20efd796c6a28d7b68249e18f4d0ee8731ce63
Yamil Torrico Vasquez <yamilvasqueztorrico@gmail.com>
2024-06-18 04:38:53 -0400

    A

--- models/nota.py
+++ models/nota.py
@@ -6,10 +6,9 @@ class Nota(models.Model):
 
     boletin_id = fields.Many2one('academico.boletin', string='Boletín')
     estudiante_id = fields.Many2one('academico.estudiante', string='Estudiante', required=True)
-    #curso_id = fields.Many2one(related='estudiante_id.curso_id', string='Curso')
-   # materia_id = fields.Many2one('academico.materia', string='Materia', required=True)
-    curso_id = fields.Many2one("academico.curso", string='Curso')
-    materia_id = fields.Many2one(related='curso_id.materia_ids', string='Materia', required=True)
+    curso_id = fields.Many2one(related='estudiante_id.curso_id', string='Curso')
+    materia_id = fields.Many2one('academico.materia', string='Materia', required=True)
+    
     nota = fields.Float(string='Nota', required=True)
     trimestre = fields.Selection([('1', '1er Trimestre'), ('2', '2do Trimestre'), ('3', '3er Trimestre')], string='Trimestre', required=True)
     anio= fields.Date(string='año', required=True)
